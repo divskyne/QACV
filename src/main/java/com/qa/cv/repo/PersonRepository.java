bd27c04de674ffcc65438022946b64ef446748f2
Melvin <melvin.kamau@academytrainee.com>
2018-10-18 13:46:03 +0100

    solving merge conflict with integration test

--- src/main/java/com/qa/cv/repo/PersonRepository.java
+++ src/main/java/com/qa/cv/repo/PersonRepository.java
@@ -2,6 +2,7 @@
 package com.qa.cv.repo;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.repository.query.Param;
@@ -12,6 +13,8 @@ import com.qa.cv.model.Person;
 @RepositoryRestResource(collectionResourceRel = "people", path = "people")
 public interface PersonRepository extends MongoRepository<Person, String> {
 
+
+	//	Optional<Person> findByName(@Param("name") String name);
 	List<Person> findByEmail(@Param("email") String email);
 
 }
--- src/test/java/intergrationTest/IntergrationTest.java
+++ src/test/java/intergrationTest/IntergrationTest.java
@@ -1,15 +1,21 @@
 package intergrationTest;
 
 import static org.hamcrest.CoreMatchers.is;
+import static org.springframework.test.web.client.response.MockRestResponseCreators.withStatus;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -36,35 +42,137 @@ public class IntergrationTest {
 	private PersonRepository myRepo;
 	
 	@Before
-	public void clearDB() {
-		myRepo.deleteAll();
+	public void clearDB() throws Exception {
+		mvc.perform(post("/api/people")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content("{\"email\":\"me@example.com\",\"name\":\"melvin\",\"role\":\"trainer\",\"password\":\"melvin\",\"cv\":\" \",\"state\":\"melvin\"}"));
+//		myRepo.deleteAll();
+//		ReportFile.createReport();
 }
-	@After
-	public void clearDBED() {
-		
-		myRepo.deleteAll();
-	}
-	
+//	@After
+//	public void clearDBED() {
+//		ReportFile.endReport;
+//		ReportFile.flushReport
+//		myRepo.deleteAll();
+//	}
+////	
 	@Test
-	public void findingAndRetrieveingPersonFromDatabase()throws Exception{
-		//myRepo.save(new Person("me@example.com", "melvin","trainee","1234"," ","approved"));
+//	@Ignore
+	public void findingAndRetrieveingPeopleFromDatabase()throws Exception{
+		//ReportFile.createTest("Finding all the people in the system");
+		
 		mvc.perform(get("/api/people")
 		.contentType(MediaType.APPLICATION_JSON))
-//		.content("{\"name\":\"Robert\",\"address\":\"Atlantis\",\"age\":200}"))
 		.andExpect(status().isOk())
 		.andExpect(content()
 				.contentTypeCompatibleWith(MediaType.APPLICATION_JSON));
-
+		
+//		if(status().equals(200))
+//		{
+//			ReportFile.LogStatusTest(Logstatus.PASS, "All people have been retrieved");
+//		}
+//		else
+//		{
+//			ReportFile.LogStatusTest(Logstatus.FAIL, "There is an error in retrieving people");
+//		}
+	}
+//	@Ignore
+	@Test
+	public void findAndRetrieveAPersonFromDatabase () throws Exception{
+		//ReportFile.createTest("Finding a person in the database");
+		List<Person> pList =myRepo.findByEmail("me@example.com");
+		for(Person p:pList) {
+		String id = p.getId();
+		 mvc.perform(get("/api/people/"+ id)
+		.contentType(MediaType.APPLICATION_JSON))
+		.andExpect(status().isOk())
+		.andExpect(jsonPath("$.email", is("me@example.com")));
+		}
+		
+//		if(status().equals(200))
+//		{
+//			ReportFile.LogStatusTest(Logstatus.PASS, "All people have been retrieved");
+//		}
+//		else
+//		{
+//			ReportFile.LogStatusTest(Logstatus.FAIL, "There is an error in retrieving people");
+//		}
+		
+		
+		
 	}
 	
 	@Test
 	public void addPersonToDatabaseTest()throws Exception{
-//		myRepo.save(new Person("me@example.com", "melvin","trainee","1234"," ","approved"));
-		mvc.perform(get("/api/people")
+
+
+//		int a=0;
+		mvc.perform(post("/api/people")
 		.contentType(MediaType.APPLICATION_JSON)
-		.content("{\"email\":\"me@example.com\",\"name\":\"melvin\",\"role\":\"melvin\",\"password\":\"melvin\",\"cv\":\"melvin\",\"state\":\"melvin\"}"))
+		.content("{\"email\":\"me@example.com\",\"name\":\"melvin\",\"role\":\"trainer\",\"password\":\"melvin\",\"cv\":\" \",\"state\":\"melvin\"}"))
 		.andExpect(status().isOk())
 		.andExpect(content()
 				.contentTypeCompatibleWith(MediaType.APPLICATION_JSON));
+
+		
+		
+//		for(Person p: myRepo.findByEmail("me@example.com")) {
+//			if(p.getEmail().equals("me@example.com")) {
+//				ReportFile.LogStatusTest(Logstatus.PASS, "Person has been added");
+//				ReportFile.LogStatusTest(Logstatus.INFO, " ");
+//			}
+//			else {
+//				ReportFile.LogStatusTest(Logstatus.FAIL, "There are no users added");
+//			}
+//		}
+
 	}
-}
+//	
+//	@Ignore
+	@Test
+	public void removeAPersonToDatabaseTest() throws Exception{
+		Long count=myRepo.count();
+		List<Person> pList =myRepo.findByEmail("me@example.com");
+		for(Person p:pList) 
+		{
+		String id = p.getId();
+		mvc.perform(delete("/api/people/"+ id)
+				.contentType(MediaType.APPLICATION_JSON))
+		.andExpect(status().isOk());
+		}
+		
+//		if(myRepo.count()==(count-1))
+//		{
+//			ReportFile.LogStatusTest(Logstatus.PASS, "Person has been removed");
+//		}
+//		else
+//		{
+//			ReportFile.LogStatusTest(Logstatus.FAIL, "There are no users have been removed");
+//		}
+//		}
+////	
+	}
+
+	@Test
+	public void updateAPersonToDatabaseTest() throws Exception{
+		
+		List<Person> updateList =myRepo.findByEmail("me@example.com");
+		for(Person p:updateList) {
+		String id = p.getId();
+		mvc.perform(put("/api/people/"+ id)
+				.contentType(MediaType.APPLICATION_JSON)
+				.content("{\n" + 
+						"        \"id\": \""+id+"\",\n" + 
+						"        \"email\": \"D\",\n" + 
+						"        \"name\": \"test1\",\n" + 
+						"        \"role\": \"trainer\",\n" + 
+						"        \"password\": \"melvin\",\n" + 
+						"        \"cv\": \"{ }\",\n" + 
+						"        \"state\": \"melvin\",\n" + 
+						"        \"docType\": null,\n" + 
+						"        \"file\": null\n" + 
+						"    }"))
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("$.email", is("D")));}
+	}
+	}
